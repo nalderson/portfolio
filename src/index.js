40fa368d72a00481c6e5f61accd2d2149da76186
Tash Alderson <aldersonnatasha@gmail.com>
2021-04-06 17:37:14 +0100

    font update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import './fonts/Britanica-Black.ttf'
+import './fonts/Britanica-Bold.ttf'
 import './styles/style.scss'
 ReactDOM.render(<App/>, document.getElementById('root'))
