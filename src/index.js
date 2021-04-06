d1b2907588511d80a1aa1947e11d3290ff505dc7
Tash Alderson <aldersonnatasha@gmail.com>
2021-04-06 15:41:00 +0100

    font update

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './fonts/britanica-black.woff'
-
+import './styles/style.scss'
 ReactDOM.render(<App/>, document.getElementById('root'))
