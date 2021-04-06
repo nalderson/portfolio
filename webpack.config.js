2626325ff7176d55759317f0f83eee3d86dc0061
Tash Alderson <aldersonnatasha@gmail.com>
2021-04-06 19:53:33 +0100

    changed webpack, import updated src url, deleted font static file

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = webpackEnv => {
         // }
         {
           test: /\.(woff2|woff|eot|ttf|otf)$/,
-          use: ['file-loader']
+          use: 'file-loader'
         }
       ]
     },
