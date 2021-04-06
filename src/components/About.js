4712bdf364244588b50782a36b286abfa8e80a5e
Tash Alderson <aldersonnatasha@gmail.com>
2021-04-06 11:45:16 +0100

    prof image

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ export default function About() {
       <div className="about-me">
         <p className="about-me-info">I am a Junior Full-Stack Software Developer with experience as an Advertising Producer where I worked in both London and New York running big Digital and TV campaigns.</p>
       </div>
-      <img className="profile-pic"src={profile}></img>
+      <img className="profile-pic"src={`/${profile}`}></img>
       <p className="about-email">aldersonnatasha@gmail.com</p>
       <p className="skills">JavaScript | ReactJS | HTML5 | CSS | SCSS | Python | Flask | Express | Node.js | Mongoose | MongoDB | Git | Github | Jest | Mocha | <br></br>Chai | pytest | ProgresSQL | Babel | Chrome | Heroku | NPM | Safari | Visual Studio Code | SSH | SQLAlchemy | React-Testing-Library</p>
     </div>
